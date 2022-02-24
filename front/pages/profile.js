4db5e6c21c8b509c8e23a33665b1b1b0b276adde
onezeun <78632052+onezeun@users.noreply.github.com>
2022-02-25 00:52:34 +0900

    프로필 페이지

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
 
+import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
+  const followerList = [{ nickname: '하찌' }, { nickname: '바보' }, { nickname: '멍청이' }]
+  const followingList = [{ nickname: '하찌' }, { nickname: '바보' }, { nickname: '멍청이' }]
+
   return (
     <>
       <Head>
         <title>내 프로필 | 하찌네</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
