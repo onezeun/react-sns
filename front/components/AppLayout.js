2286f4a6fc6b34de50c10c347904c12826b65dae
onezeun <78632052+onezeun@users.noreply.github.com>
2022-02-24 18:55:49 +0900

    LoginFrom

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Menu, Input, Row, Col } from 'antd';
+
+import UserProfile from './UserProfile'
+import LoginForm from './LoginForm'
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState('false'); // 더미데이터
+
   return (
     <div>
-      <div>공통 메뉴</div>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>하찌네</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}> {/* gutter : 컬럼 사이의 간격 */ }
+        <Col xs={24} md={6}>
+          <LoginForm />
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://velog.io/@onezeun" target="_blank" rel="noreferrer noopener">Made by onezeun</a>
+        </Col>
+      </Row>
     </div>
-  )
+  );
 };
 
 AppLayout.propTypes = {
